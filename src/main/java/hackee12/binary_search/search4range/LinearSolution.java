3f6ebe63eea178bed8b203f98db4e07a15472a69
hackee12 <hackee.owl@gmail.com>
2020-10-10 18:38:52 -0400

    Add binary_search.search4range.LinearSolution

---
+++ src/main/java/hackee12/binary_search/search4range/LinearSolution.java
@@ -0,0 +1,16 @@
+package hackee12.binary_search.search4range;
+
+public class LinearSolution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[]{-1,-1};
+        if (nums.length != 0) {
+            int right = -1;
+            int i = 0;
+            while (i < nums.length && nums[i] < target) i++;
+            if (i < nums.length && nums[i] == target) res[0] = i;
+            while (i+1 < nums.length && nums[i+1] == target) i++;
+            if (i < nums.length && nums[i] == target) res[1] = i;
+        }
+        return res;
+    }
+}
