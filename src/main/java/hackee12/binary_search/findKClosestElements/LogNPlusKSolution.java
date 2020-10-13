0901b32f5b0fd4b4b4f9e5cce652f1ff3d2b4691
hackee12 <hackee.owl@gmail.com>
2020-10-13 00:32:21 -0400

    Reformat binary_search.findKClosestElements.LogNPlusKSolution

--- src/main/java/hackee12/binary_search/findKClosestElements/LogNPlusKSolution.java
+++ src/main/java/hackee12/binary_search/findKClosestElements/LogNPlusKSolution.java
@@ -6,19 +6,19 @@ import java.util.List;
 public class LogNPlusKSolution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int left = 0;
-        int right = arr.length-1;
+        int right = arr.length - 1;
         while (left < right) {
-            int mid = (left+right)>>>1;
+            int mid = (left + right) >>> 1;
             if (arr[mid] == x) {
                 left = mid;
                 break;
             } else if (arr[mid] > x) {
-                right = mid-1;
+                right = mid - 1;
             } else {
-                left = mid+1;
+                left = mid + 1;
             }
         }
-        if (arr[left] != x && left-1 >= 0 && x - arr[left-1] <= arr[left] - x) {
+        if (arr[left] != x && left - 1 >= 0 && x - arr[left - 1] <= arr[left] - x) {
             left -= 1;
         }
         LinkedList<Integer> reply = new LinkedList<>();
@@ -27,19 +27,19 @@ public class LogNPlusKSolution {
         int i = 1;
         int j = 1;
         while (k-- > 0) {
-            if (left-i >= 0 && left+j < arr.length) {
-                if (x - arr[left-i] <= arr[left+j] - x) {
-                    reply.addFirst(arr[left-i]);
+            if (left - i >= 0 && left + j < arr.length) {
+                if (x - arr[left - i] <= arr[left + j] - x) {
+                    reply.addFirst(arr[left - i]);
                     i++;
                 } else {
-                    reply.addLast(arr[left+j]);
+                    reply.addLast(arr[left + j]);
                     j++;
                 }
-            } else if (left-i >= 0) {
-                reply.addFirst(arr[left-i]);
+            } else if (left - i >= 0) {
+                reply.addFirst(arr[left - i]);
                 i++;
             } else {
-                reply.addLast(arr[left+j]);
+                reply.addLast(arr[left + j]);
                 j++;
             }
         }
