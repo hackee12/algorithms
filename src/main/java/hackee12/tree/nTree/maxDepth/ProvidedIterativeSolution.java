12c8b94eafa8a905a3ab6badd46f6f53aa8b1a22
hackee12 <hackee.owl@gmail.com>
2021-03-21 10:35:46 -0400

    Use own Pair

--- src/main/java/hackee12/tree/nTree/maxDepth/ProvidedIterativeSolution.java
+++ src/main/java/hackee12/tree/nTree/maxDepth/ProvidedIterativeSolution.java
@@ -1,7 +1,7 @@
 package hackee12.tree.nTree.maxDepth;
 
+import hackee12.Pair;
 import hackee12.tree.nTree.Node;
-import jdk.internal.vm.compiler.collections.Pair;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -12,18 +12,18 @@ public class ProvidedIterativeSolution implements Solution {
     public int maxDepth(Node root) {
         Queue<Pair<Node, Integer>> stack = new LinkedList<>();
         if (root != null) {
-            stack.add(Pair.create(root, 1));
+            stack.add(new Pair<>(root, 1));
         }
 
         int depth = 0;
         while (!stack.isEmpty()) {
             Pair<Node, Integer> current = stack.poll();
-            root = current.getLeft();
-            int current_depth = current.getRight();
+            root = current.fst;
+            int current_depth = current.snd;
             if (root != null) {
                 depth = Math.max(depth, current_depth);
                 for (Node c : root.children) {
-                    stack.add(Pair.create(c, current_depth + 1));
+                    stack.add(new Pair<>(c, current_depth + 1));
                 }
             }
         }
