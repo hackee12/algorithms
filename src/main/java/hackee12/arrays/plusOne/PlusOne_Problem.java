958ead7cffc3e806b95a7cfcf95f28a627e4993e
hackee12 <hackee.owl@gmail.com>
2022-04-07 23:36:50 -0400

    Track PlusOne_Problem

---
+++ src/main/java/hackee12/arrays/plusOne/PlusOne_Problem.java
@@ -0,0 +1,13 @@
+package hackee12.arrays.plusOne;
+
+/**
+ * A non-empty integer array 'digits' represents a non-negative decimal number.
+ * The most significant digit is at the head of the array.
+ * And each element represents a single digit.
+ * There will be no leading zeros except the number 0 itself.
+ * Implement interface so that plusOne() returns an array of digits
+ * which represents input decimal incremented by 1.
+ */
+public interface PlusOne_Problem {
+    int[] plusOne(int[] digits);
+}
