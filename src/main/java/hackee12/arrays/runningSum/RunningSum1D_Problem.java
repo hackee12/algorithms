289ade701f884ec54e2f6ab54fc13c1b42e2d422
hackee12 <hackee.owl@gmail.com>
2023-02-03 14:01:00 -0500

    Document limitations for RunningSum1D_Problem

--- src/main/java/hackee12/arrays/runningSum/RunningSum1D_Problem.java
+++ src/main/java/hackee12/arrays/runningSum/RunningSum1D_Problem.java
@@ -2,6 +2,7 @@ package hackee12.arrays.runningSum;
 
 /**
  * Return a running sum of an array as runningSum[i] = nums[0] + nums[1] + .. + nums[i].
+ * Limitations: 1 <= nums.length <= 1000, -10^6 <= nums[i] <= 10^6.
  */
 public interface RunningSum1D_Problem {
     int[] runningSum(int[] nums);
